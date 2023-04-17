09c9f55085557c3e6c59f8e4fd695810eeda5813
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-04-17 17:44:39 +0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/main.css";
 import "./styles/reset.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
@@ -12,30 +12,28 @@ import ScrollToTop from "./utils/scrollToTop.js";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <ScrollToTop />
-        <NavBar />
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/projects"
-            element={<Projects />}
-          />
-          <Route
-            path="/project/:id"
-            element={<Project />}
-          />
-          <Route
-            path="/contacts"
-            element={<Contacts />}
-          />
-        </Routes>
+      <ScrollToTop />
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/projects"
+          element={<Projects />}
+        />
+        <Route
+          path="/project/:id"
+          element={<Project />}
+        />
+        <Route
+          path="/contacts"
+          element={<Contacts />}
+        />
+      </Routes>
 
-        <Footer />
-      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
